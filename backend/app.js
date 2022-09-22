e5a903c28b8790bb421e5f125d9f799c21d21d13
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-09-22 08:51:15 -0300

    Initial settings for backend

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+const port = process.env.APP_PORT;
+
+// JSON response
+app.use(express.json());
+
+// CORS
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000" /* IP da aplicação frontend */,
+  })
+);
+
+// Public folder
+app.use(express.static(__dirname + "/public"));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Testing" });
+});
+
+// Starting App
+app.listen(port, () => {
+  console.log(`* API is running at port: ${port}`);
+  console.log(`* http://localhost:${port}`);
+});
