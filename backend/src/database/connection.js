3ee44a67b7d6f369c97f122c809ea7a7b620f303
Claudio Henrique <claudio_henrique20@hotmail.com>
2022-09-23 11:29:35 -0300

    Adding mongodb connection

---
+++ backend/src/database/connection.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+const main = async () => {
+  await mongoose.connect(process.env.DB_UR);
+};
+
+main()
+  .then(() => {
+    console.log("* Database successfully connected");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+module.exports = mongoose;
